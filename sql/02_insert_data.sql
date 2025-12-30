672580f1ef715e7e493208ac5c172922bc8aacbb
Marthinmezaa <juanmameza10@hotmail.com>
2025-12-30 15:46:30 -0300

    Module 2

---
+++ sql/02_insert_data.sql
@@ -0,0 +1,12 @@
+-- ============================
+-- MÓDULO 2: INSERCIÓN DE DATOS
+-- ============================
+
+INSERT INTO estudiantes (nombre, edad, carrera)
+VALUES ('Martin', 25, 'Medicina');
+
+INSERT INTO estudiantes (nombre, edad, carrera)
+VALUES ('Ana', 22, 'Ingenieria');
+
+INSERT INTO estudiantes (nombre, edad, carrera)
+VALUES ('Carlos', 28, 'Derecho');
