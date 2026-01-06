4d3090ed62d71ca5039aea78fa054139632ec4cb
Marthinmezaa <juanmameza10@hotmail.com>
2026-01-06 11:25:15 -0300

    Module 3

---
+++ sql/03_select_basics.sql
@@ -0,0 +1,7 @@
+-- ============================
+-- MÓDULO 3: SELECT BÁSICO
+-- ============================
+
+SELECT nombre FROM estudiantes;
+SELECT * FROM estudiantes WHERE edad < 26;
+SELECT nombre FROM estudiantes ORDER BY nombre ASC;
